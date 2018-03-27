8ecd7a196835506e90cd87500a6c1cbe91c37128
ecacerest <ecacerest@finagil.com.mx>
2018-03-27 13:37:27 -0600

    Seguimiento ya no solo es de credito, es para otras areas

--- Mod_CRED.vb
+++ Mod_CRED.vb
@@ -7,18 +7,18 @@
         Dim Resposble As String = ""
         Dim Asunto As String = ""
         Dim Mensaje As String = ""
-        Dim Tipo As String
+
 
         Select Case Opcion.ToUpper
             Case "VENCIDO"
                 solicitudes.Fill_Vencidos(tsol)
-                Asunto = "Notificación de Seguimiento de Crédito: VENCIDOS"
+                Asunto = "Notificación de Seguimiento: VENCIDOS"
             Case "DIA"
                 solicitudes.Fill_PorVencer(tsol, Dias)
                 If Dias = 0 Then
-                    Asunto = "Notificación de Seguimiento de Crédito: VENCEN HOY "
+                    Asunto = "Notificación de Seguimiento: VENCEN HOY "
                 Else
-                    Asunto = "Notificación de Seguimiento de Crédito: VENCE EN " & Dias & " DIAS."
+                    Asunto = "Notificación de Seguimiento: VENCE EN " & Dias & " DIAS."
                 End If
         End Select
 
@@ -30,9 +30,11 @@
             Mensaje += "Días de Retraso: " & r.DiasRetraso & "<br>"
             Mensaje += "Notas: " & r.Notas & "<br>"
             Resposble = CORREOS.ScalarCorreo(r.Responsable)
-            EnviacORREO(Resposble, Mensaje, Asunto, "SeguimientoCREDITO@finagil.com.mx")
+            EnviacORREO(Resposble, Mensaje, Asunto, "Seguimiento@finagil.com.mx")
+            Resposble = CORREOS.ScalarCorreo(r.Analista)
+            EnviacORREO(Resposble, Mensaje, Asunto, "Seguimiento@finagil.com.mx")
         Next
-        EnviacORREO("ecacerest@finagil.com.mx", Mensaje, Asunto, "SeguimientoCREDITO@finagil.com.mx")
+        EnviacORREO("ecacerest@finagil.com.mx", Mensaje, Asunto, "Seguimiento@finagil.com.mx")
 
     End Sub
 
@@ -46,9 +48,9 @@
 
         solicitudes.Fill_Sucursal(tsol, Sucursal)
         If tsol.Rows.Count > 0 Then
-            Asunto = "Notificación Semanal de Seguimiento de Crédito."
+            Asunto = "Notificación Semanal de Seguimiento."
             Mensaje = "<table BORDER=1><tr><td><strong>Contrato</strong></td><td><strong>Cliente</strong></td><td><strong>Responsable</strong></td><td><strong>Compromiso</strong></td>" _
-            & "<td><strong>Días de Retraso</strong></td><td><strong>Notas</strong></td><td></tr>"
+            & "<td><strong>Días de Retraso</strong></td><td><strong>Area</strong></td><td><td><strong>Notas</strong></td><td></tr>"
 
             For Each r As ProduccionDS.CRED_SeguimientosRow In tsol.Rows
                 Mensaje += "<tr><td>" & r.Anexo & "</td>"
@@ -56,15 +58,22 @@
                 Mensaje += "<td>" & r.Responsable & "</td>"
                 Mensaje += "<td>" & r.Compromiso & "</td>"
                 Mensaje += "<td>" & r.DiasRetraso & "</td>"
+                If r.Analista = "araceli.sanchez" Then
+                    Mensaje += "<td>Seguros</td>"
+                Else
+                    Mensaje += "<td>Crédito</td>"
+                End If
                 Mensaje += "<td>" & r.Notas & "</td></tr>"
+                Resposble = CORREOS.ScalarCorreo(r.Analista)
             Next
             Mensaje += "</table>"
             CORREOS_FASE.Fill(TMAIL, "JEFE_" & Sucursal)
             For Each rrr As ProduccionDS.CorreosFasesRow In TMAIL.Rows
-                EnviacORREO(rrr.Correo, Mensaje, Asunto, "SeguimientoCREDITO@finagil.com.mx")
+                EnviacORREO(rrr.Correo, Mensaje, Asunto, "Seguimiento@finagil.com.mx")+9
             Next
+            EnviacORREO(Resposble, Mensaje, Asunto, "Seguimiento@finagil.com.mx")
         End If
-        EnviacORREO("ecacerest@finagil.com.mx", Mensaje, Asunto, "SeguimientoCREDITO@finagil.com.mx")
+        EnviacORREO("ecacerest@finagil.com.mx", Mensaje, Asunto, "Seguimiento@finagil.com.mx")
     End Sub
 
 End Module

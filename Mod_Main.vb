15a23793023da310d2ca4ea550b68948f9aeb75c
ecacerest <ecacerest@finagil.com.mx>
2018-04-11 10:14:30 -0500

    cambio de 6 am a 7 am

--- Mod_Main.vb
+++ Mod_Main.vb
@@ -9,7 +9,7 @@ Module Mod_Main
             EnviaCorreoAVIO()
             Console.WriteLine("Seguimiento de Crédito")
             Console.WriteLine(Date.Now.Hour)
-            If Date.Now.Hour = 6 And Date.Now.Minute < 5 Then 'se ejecutan una sola ves al dia a las 6 am
+            If Date.Now.Hour = 7 And Date.Now.Minute = 10 Then 'se ejecutan una sola ves al dia a las 6 am
                 EnviaCorreoPAGOS_PASIVO(Date.Now.Date)
                 EnviaCorreoSEGUI_CRED("DIA", -3)
                 EnviaCorreoSEGUI_CRED("DIA", 0)

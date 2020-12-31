bc1aac65c12c95145809dffeb8e9f9951f86a508
ecacerest <ecacerest@finagil.com.mx>
2020-12-30 20:14:42 -0600

    se quito a Mbautista de Riesgos

--- Mod_Tasas.vb
+++ Mod_Tasas.vb
@@ -18,7 +18,7 @@
             Mensaje += "Producto: " & r.TipoCredito & "<br>"
             Asunto = "Se requiere Validacion de porcentaje Reserva (" & r.Cliente.Trim & ")"
             taMail.Insert(De, "ajoshin@finagil.com.mx", Asunto, Mensaje, False, Date.Now, "")
-            taMail.Insert(De, "maria.bautista@finagil.com.mx", Asunto, Mensaje, False, Date.Now, "")
+            'taMail.Insert(De, "maria.bautista@finagil.com.mx", Asunto, Mensaje, False, Date.Now, "")
             taMail.Insert(De, "ecacerest@finagil.com.mx", Asunto, Mensaje, False, Date.Now, "")
             Btasas.Enviados(True, r.id)
         Next
@@ -40,20 +40,30 @@
             Mensaje += "Producto: " & r.TipoCredito & "<br>"
             Mensaje += "Indicadores: " & r.Indicadores & "<br>"
             taMail.Insert(De, "ajoshin@finagil.com.mx", Asunto, Mensaje, False, Date.Now, "")
-            taMail.Insert(De, "maria.bautista@finagil.com.mx", Asunto, Mensaje, False, Date.Now, "")
+            'taMail.Insert(De, "maria.bautista@finagil.com.mx", Asunto, Mensaje, False, Date.Now, "")
             taMail.Insert(De, "ecacerest@finagil.com.mx", Asunto, Mensaje, False, Date.Now, "")
             Mensaje = "Contrato: " & r.AnexoCon & "<br>"
             Mensaje += "Cliente: " & r.Cliente.Trim & "<br>"
             Mensaje += "Promotor: " & r.Promotor & "<br>"
             Mensaje += "Producto: " & r.TipoCredito & "<br>"
             Mensaje += "<A HREF='https://finagil.com.mx/WEBtasas/951sb999-7xx8.aspx?ID=" & r.id & "'>Liga de Autorización</A>"
-            If r.Nombre_Sucursal.Trim = "NAVOJOA" Or r.Nombre_Sucursal.Trim = "MEXICALI" Then
-                taMail.Insert(De, "mleal@finagil.com.mx", "Notificación de Tasa Especial (" & r.Cliente.Trim.Substring(0, 100) & ")", Mensaje, False, Date.Now, "")
+            If r.Cliente.Trim.Length > 100 Then
+                If r.Nombre_Sucursal.Trim = "NAVOJOA" Or r.Nombre_Sucursal.Trim = "MEXICALI" Then
+                    taMail.Insert(De, "mleal@finagil.com.mx", "Notificación de Tasa Especial (" & r.Cliente.Trim.Substring(0, 100) & ")", Mensaje, False, Date.Now, "")
+                Else
+                    taMail.Insert(De, "mleal@finagil.com.mx", "Notificación de Tasa Especial (" & r.Cliente.Trim.Substring(0, 100) & ")", Mensaje, False, Date.Now, "")
+                End If
+                taMail.Insert(De, "ecacerest@lamoderna.com.mx", "Notificación de Tasa Especial (" & r.Cliente.Trim.Substring(0, 100) & ")", Mensaje, False, Date.Now, "")
             Else
-                taMail.Insert(De, "mleal@finagil.com.mx", "Notificación de Tasa Especial (" & r.Cliente.Trim.Substring(0, 100) & ")", Mensaje, False, Date.Now, "")
+                If r.Nombre_Sucursal.Trim = "NAVOJOA" Or r.Nombre_Sucursal.Trim = "MEXICALI" Then
+                    taMail.Insert(De, "mleal@finagil.com.mx", "Notificación de Tasa Especial (" & r.Cliente.Trim & ")", Mensaje, False, Date.Now, "")
+                Else
+                    taMail.Insert(De, "mleal@finagil.com.mx", "Notificación de Tasa Especial (" & r.Cliente.Trim & ")", Mensaje, False, Date.Now, "")
+                End If
+                taMail.Insert(De, "ecacerest@lamoderna.com.mx", "Notificación de Tasa Especial (" & r.Cliente.Trim & ")", Mensaje, False, Date.Now, "")
+
             End If
 
-            taMail.Insert(De, "ecacerest@lamoderna.com.mx", "Notificación de Tasa Especial (" & r.Cliente.Trim.Substring(0, 100) & ")", Mensaje, False, Date.Now, "")
             Btasas.Enviados(True, r.id)
         Next
         Btasas.FillByGD(bt)

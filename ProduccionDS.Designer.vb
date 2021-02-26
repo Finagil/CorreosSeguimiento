7e555e78818258a4b672def4bc83f7039160aa27
ecacerest <ecacerest@finagil.com.mx>
2021-02-26 13:34:30 -0600

    Gen_Correos_SistemaFinagil Asunto a 150 caracteres

--- ProduccionDS.Designer.vb
+++ ProduccionDS.Designer.vb
@@ -8754,7 +8754,7 @@ Partial Public Class ProduccionDS
             Me.columnid_Correo.Unique = true
             Me.columnDe.MaxLength = 200
             Me.columnPara.MaxLength = 400
-            Me.columnAsunto.MaxLength = 100
+            Me.columnAsunto.MaxLength = 150
             Me.columnMensaje.MaxLength = 4000
             Me.columnfecha.AllowDBNull = false
             Me.columnAttach.MaxLength = 400
